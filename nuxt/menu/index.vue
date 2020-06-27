e8b5a2b1d2d0498b69992a30441303e3c288c278
b01110011 <b01110011b@gmail.com>
2020-06-27 18:29:03 +0700

    вывод меню из хранилища

---
+++ nuxt/menu/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="menu d-flex flex-column">
+        <div
+            v-for="item in $store.getters.menu"
+            :key="item.name"
+        >
+
+            <n-link
+                v-if="item.link"
+                :to="item.link"
+            >
+                <v-icon left>{{ item.icon }}</v-icon>
+                {{ item.name }}
+            </n-link>
+            <a
+                v-else
+                @click.prevent="$store.commit(item.click, true)"
+            >
+                <v-icon left>{{ item.icon }}</v-icon>
+                {{ item.name }}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
